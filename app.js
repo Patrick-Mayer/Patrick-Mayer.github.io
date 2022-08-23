24e3bba188f9313ff62ab2f33cd45c5cef14ee31
Patrick Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2022-08-22 22:02:59 -0400

    Wrong repo, lmao

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+TODO:
+*Create Resume
+*Apply grid to footer to center it and add space in between icons.
+*Implement Tabs System for Navbar
+*Add email button thing
+*Center paragraph and make it look clean
+
+Brainstorm:
+Themes - Terminal, USA, Holidays, 
+*/
