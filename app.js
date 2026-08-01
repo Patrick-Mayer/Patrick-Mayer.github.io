ff7c6705f8437b17874d794eba7b900cc8818029
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2026-08-01 18:34:23 -0400

    first push to terminal branch

--- app.js
+++ app.js
@@ -1,25 +1,73 @@
-/*
-TODO:
+//I despise that I'm still using this repulsive language
 
-*Apply grid to footer to center it and add space in between icons.
-*Implement Tabs System for Navbar
-*Center paragraph and make it look clean, make border a bunch of programming language icons, maybe make them move clockwise
+const BOOTUP_SFX = new Audio("Assets/Bootup.mp3");
+const BLOCK_CURSOR = document.getElementById("BlockCursorImage");
 
-Brainstorm:
-Themes - Terminal, USA, Holidays, 
-*/
+var startTime = Date.now();
+var interactedWithPage = false;
 
 function copyToClipboard(input) {
-    
-    //for email cause HTML reads '@' as special character
-    if (input === 'e'){
-        var copy = "pmayer1278@floridapoly.edu";
-        navigator.clipboard.writeText(copy);
-        alert("Coppied to clipboard");
-    }
-    else{
-        var copy = input;
-        navigator.clipboard.writeText(copy);
-        alert("Coppied to clipboard");
-    }
+  //for email cause HTML reads '@' as special character
+  if (input === "e") {
+    navigator.clipboard.writeText("patrick.mayer2003@protonmail.com");
+    alert("Coppied to clipboard");
+  } else {
+    navigator.clipboard.writeText(input);
+    alert("Coppied to clipboard");
+  }
 }
+
+function Wait(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function BootSequence() {
+  //needed so duration isn't null
+  await new Promise((resolve) => {
+    BOOTUP_SFX.addEventListener("loadedmetadata", resolve, { once: true });
+  });
+  await Wait(BOOTUP_SFX.duration * 1000);
+
+  document.querySelector(".navbar").classList.remove("hidden");
+}
+
+//main()
+document.addEventListener("DOMContentLoaded", () => {
+  BOOTUP_SFX.preload = "auto";
+  //BLOCK_CURSOR.style.left = "400px";
+  //BLOCK_CURSOR.style.top = "400px";
+
+  BootSequence(); //needs to be it's own function cause of async
+});
+
+document.addEventListener("click", async () => {
+  if (interactedWithPage) {
+    return;
+  }
+
+  const MILLISECONDS_PER_SECOND = 1000;
+  var deltaTime = (Date.now() - startTime) / MILLISECONDS_PER_SECOND;
+  BOOTUP_SFX.addEventListener("loadedmetadata", () => {
+    BOOTUP_SFX.currentTime = Math.min(deltaTime, BOOTUP_SFX.duration);
+    BOOTUP_SFX.play();
+  });
+  BOOTUP_SFX.load();
+
+  interactedWithPage = true;
+});
+
+const nameEl = document.getElementById("name");
+let nameState = "";
+nameEl.innerHTML = nameState;
+const nameLetters = ["P", "a", "t", "r", "i", "c", "k"];
+
+const intervalId = setInterval(() => {
+  nameState += nameLetters.shift();
+  nameEl.innerHTML = nameState;
+
+  if (nameLetters.length === 0) {
+    clearInterval(intervalId);
+  }
+}, 1000);
+
+BOOTUP_SFX.addEventListener;
